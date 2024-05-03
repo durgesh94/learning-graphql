027658009ffa0388daaf3821d4afe111656ca6f6
Durgesh Tambe <dntambe@gmail.com>
2024-05-03 21:23:24 +0530

    graphQL Playground

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
-import { ruruHTML } from "ruru/server"
+import { ruruHTML } from "ruru/server";
+// const expressPlayground = require('graphql-playground-middleware-express').default
+import expressPlayground from 'graphql-playground-middleware-express';
 import schema from './src/data/schema.js';
 import resolvers from './src/data/resolvers.js';
+
+const graphqlPlayground = expressPlayground.default;
 
 const PORT = 8080;
 
@@ -20,6 +24,9 @@ app.all("/graphql", createHandler({
 app.get("/", (_req, res) => {
     res.type("html")
     res.end(ruruHTML({ endpoint: "/graphql" }))
-})
+});
+
+// GraphQL Playground
+app.get('/playground', graphqlPlayground({ endpoint: '/graphql' }));
 
 app.listen(PORT, () => console.log(`Running server on localhost:${PORT}/graphql`));
